d8981265ebe0a74dfb850636c41927f10dd6d2b5
nikkho <nikkho@ddd8a7e7-04a1-4c0d-ae79-48073a28b335>
2021-09-11 10:48:01 +0000

    Upgraded to C++ Builder 11 (Alexandria)

--- trunk/FileOptimizer/Source/cppAbout.cpp
+++ trunk/FileOptimizer/Source/cppAbout.cpp
@@ -77,7 +77,9 @@ void __fastcall TfrmAbout::FormCreate(TObject *Sender)
 	#elif ((__BORLANDC__ >= 0x0750) && (__BORLANDC__ <= 0x0759))
 		imgAbout->Hint = "Embarcadero C++ Builder 10.4 Sydney";
 	#elif ((__BORLANDC__ >= 0x0760) && (__BORLANDC__ <= 0x0769))
-		imgAbout->Hint = "Embarcadero C++ Builder 10.5";
+		imgAbout->Hint = "Embarcadero C++ Builder 11.0 Alexandria";
+	#elif ((__BORLANDC__ >= 0x0770) && (__BORLANDC__ <= 0x0779))
+		imgAbout->Hint = "Embarcadero C++ Builder 12.0";
 	#else
 		imgAbout->Hint = "Borland/Inprise/CodeGear/Embarcadero C++ Builder";
 	#endif
