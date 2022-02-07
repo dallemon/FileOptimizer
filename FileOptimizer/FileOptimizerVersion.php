48deeb9b314284da2639115396944f5c557ad697
Javier Gutiérrez Chamorro <4672971+javiergutierrezchamorro@users.noreply.github.com>
2022-02-07 11:42:31 +0000

    15.40

--- FileOptimizer/FileOptimizerVersion.php
+++ FileOptimizer/FileOptimizerVersion.php
@@ -77,7 +77,7 @@
 		'ShowToolBar' => false,
 		'HideAds' => false,
 		'TempDirectory' => '',
-		'Version' => '15.30.2661',
+		'Version' => '15.40.2677',
 		'Time' => 0,
 		'Opens' => 0,
 		'Files' => 0,
